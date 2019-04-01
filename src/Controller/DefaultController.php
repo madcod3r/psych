a0ca1ba8ca064ca50af4e3eb6138fe1f5851fc00
Олег Волошин <vololeg93@gmail.com>
2019-04-01 23:50:45 +0300

    added Default Controller and index action with template

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('default/index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
